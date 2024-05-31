c1eea2c281e0a0dcccfb44c17de8afa88974920b
Mirsha-RG <142954534+Mirsha-RG@users.noreply.github.com>
2024-05-30 18:59:08 -0600

    Update settings.py

--- lugares/settings.py
+++ lugares/settings.py
@@ -82,7 +82,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'lugar',
         'USER': 'postgres',
-        'PASSWORD': 'panchis',
+        'PASSWORD': 'xxxxxxxxxx',
         'HOST': 'localhost',
         'PORT': '5432',
     }
